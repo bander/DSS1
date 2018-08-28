82d2678e3bfdea2f9b7ebcfecbf4a010773ed4d0
valdic <yvyn@yandex.ru>
2018-08-29 00:26:42 +0400

    добавление ЮЙ в ассет стор

---
+++ Assets/Asset_Stor/Starlight Mobile UI for UGUI/Scripts/Miscellaneous/InputFocusFix.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityEngine.UI
+{
+    [RequireComponent(typeof(Input))]
+    public class InputFocusFix : MonoBehaviour
+    {
+        [SerializeField] private Color m_Color;
+        [SerializeField] private Text m_Target;
+
+        protected void Awake()
+        {
+            InputField input = this.gameObject.GetComponent<InputField>();
+
+            if (input != null)
+            {
+                input.onValueChanged.AddListener(OnValueChange);
+            }
+        }
+
+        public void OnValueChange(string str)
+        {
+            if (this.m_Target != null && string.IsNullOrEmpty(str))
+            {
+                this.m_Target.canvasRenderer.SetColor(this.m_Color);
+            }
+        }
+    }
+}
