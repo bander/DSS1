066fea217890fb0a9739de76f03d71348b9c4118
valdic <yvyn@yandex.ru>
2018-11-08 14:01:50 +0400

    нав меш и червь сцена и лайт мапы

---
+++ Assets/worm_animator_behaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worm_animator_behaviour : StateMachineBehaviour {
+
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	//override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Debug.Log("exit state ");
+
+        worm_control wc = animator.gameObject.GetComponentInParent<worm_control>();
+        if (wc != null)
+        {
+
+        }
+        else
+        {
+            Debug.Log("hmm... what the fuck?");
+            return;
+        }
+
+        if (stateInfo.IsName("worm_hide"))
+        {
+            wc.Hide();
+            return;
+        }
+
+        if (stateInfo.IsName("worm_hide"))
+        {
+            wc.Spawned();
+            return;
+        }
+    }
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
