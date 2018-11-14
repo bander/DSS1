9a1bf10abaad0437077f53b6dcc5f86d13477723
valdic <yvyn@yandex.ru>
2018-11-14 12:32:11 +0400

    лого

---
+++ Assets/Scripts/Fade_out.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade_out : MonoBehaviour {
+    void Start()
+    {
+        StartCoroutine(DoFade());
+    }
+
+    IEnumerator DoFade()
+    {
+        CanvasGroup canvasGroup = GetComponent<CanvasGroup>();
+        Image im = GetComponent<Image>();
+
+        while (im.color.a > 0)
+        {
+            var tempColor = im.color;
+            tempColor.a -= Time.deltaTime / 5;
+            im.color = tempColor;
+            yield return null;
+        }
+        GameObject.Destroy(gameObject);
+    }
+
+}
